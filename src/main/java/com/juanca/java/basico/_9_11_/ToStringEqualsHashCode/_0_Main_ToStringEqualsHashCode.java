0faf38a13e2f77d4cb2f3fac44e87723ff2aaa5a
Juan Carlos Arias <jariasp@sura.com.co>
2021-07-12 23:40:08 -0500

    Ejercicio de uso de la implementación de los métodos más sobreescritos de la clase Object

---
+++ src/main/java/com/juanca/java/basico/_9_11_/ToStringEqualsHashCode/_0_Main_ToStringEqualsHashCode.java
@@ -0,0 +1,34 @@
+package com.juanca.java.basico._9_11_.ToStringEqualsHashCode;
+
+public class _0_Main_ToStringEqualsHashCode {
+
+	public static void main(String[] args) {
+		Abogado abogado1 = new Abogado("Juan Carlos", 31, "71598763");
+		System.out.println("Abogado1: " + abogado1.toString());
+
+		Abogado abogado2 = new Abogado("Juan Carlos", 31, "71598763");
+		System.out.println("Abogado2: " + abogado2.toString());
+
+		System.out.println("Son iguales en su contenido: " + abogado1.equals(abogado2));
+
+		System.out.println();
+		// Al hacer lo siguiente estamos es comparando referencias en memoria 
+		// y no el contenido como anteriormente con equals
+		// solo se usa == cuando son tipos de datos primitivos
+
+		if (abogado1 == abogado2) 
+			System.out.println("Son iguales en referencia o en memoria");
+		else
+			System.out.println("No son iguales en referencia o en memoria");
+
+		System.out.println();
+
+		/*
+		 * Por regla, si el método equals() entre dos objetos devuelve verdadero, 
+		 * entonces el hashcode debería ser igual.
+		 */
+
+		System.out.println("Los hash son iguales: " + (abogado1.hashCode() == abogado2.hashCode()));;
+	}
+
+}
